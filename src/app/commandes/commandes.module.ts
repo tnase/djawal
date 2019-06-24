2b71844ebee25785000e31f49b53e7380fe213c4
tnase <alaintona1@gmail.com>
2019-06-24 17:06:03 +0100

    commit du projet

---
+++ src/app/commandes/commandes.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { CommandesPage } from './commandes.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: CommandesPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [CommandesPage]
+})
+export class CommandesPageModule {}
